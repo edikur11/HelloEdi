f861cd7d0c751aa67c2f1091ce82df8cc4da8b5e
edikur11 <edi11kurniawan@gmail.com>
2017-03-01 11:43:41 +0700

    perubahan port

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -12,7 +12,7 @@ namespace Service.Controllers
     public class HelloController : Controller
     {
         httpclientmodel client = new httpclientmodel();
-        string _address= "http://172.18.0.6:5001/api/selly/hello";
+        string _address= "api/selly/hello";
 
         // GET api/values
         [HttpGet]
